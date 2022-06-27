8552a957aa7aa54cb2a8b23683969ea4f617e040
yzqdev <yzqdev@outlook.com>
2022-06-27 18:47:22 +0800

    添加confg.yml

--- go-embed/main.go
+++ go-embed/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/gookit/color"
+	"go-embed/config"
 	"go-embed/public"
 	"go-embed/static"
 )
@@ -13,6 +15,9 @@ func main() {
 	// example: /public/static/js/1.js
 	public.Static(router)
 	static.Static(router)
+	WithConfStatic(router)
 	MainStatic(router)
+	conf := config.GetGlobal()
+	color.Redln(conf.Mysql.User)
 	router.Run(":9080")
 }
--- go-embed/main_static.go
+++ go-embed/main_static.go
@@ -3,12 +3,23 @@ package main
 import (
 	"embed"
 	"github.com/gin-gonic/gin"
+	"github.com/gookit/color"
 	"io/fs"
 	"net/http"
 )
 
 //go:embed   templates
 var f embed.FS
+
+//go:embed   config.yml
+var confFile embed.FS
+
+func WithConfStatic(router *gin.Engine) {
+
+	router.StaticFS("/f", http.FS(confFile))
+
+	color.Yellowln(http.FS(f))
+}
 
 func MainAssets() http.FileSystem {
 	//这一步是必须的
